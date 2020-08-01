682dab4a382cbdc0e1b91b93456285111e87e3d7
olmo <olmo1995@gmail.com>
2020-08-01 16:18:31 +0200

    imlemented formHandler function and building up index html core structure

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,42 @@
+// check what text was put into the form field
 function handleSubmit(event) {
     event.preventDefault()
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    const baseURL = "http://localhost:8081/sentimentApi";
+    const url = document.getElementById('url').value;
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    //CHECK IF URL IS VALID
+    if (validUrl(url)) {
+        fetch(baseURL, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({url: url})
+        })
+            .then(res => res.json())
+            .then(function (res) {
+                document.getElementById('polarity').innerHTML = res.polarity
+                document.getElementById('subjectivity').innerHTML = res.subjectivity
+                document.getElementById('confidence').innerHTML = res.confidence
+                document.getElementById('irony').innerHTML = res.irony
+            })
+    } else {
+        alert("The URL is not valid. Please isert another one.")
+    }
 }
 
-export { handleSubmit }
+//VAID URL FUNCTION
+function validUrl(url) {
+    var regex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+    
+    if(regex.test(url)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//EXPORT FILES
+export {handleSubmit}
