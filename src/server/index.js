8e54e6e82443f57d061fa1db0e93f1c7b4bfb16a
olmo <olmo1995@gmail.com>
2020-08-01 14:47:48 +0200

    feat:adding POST request

--- src/server/index.js
+++ src/server/index.js
@@ -2,33 +2,66 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+//Entrypoint for the data
+let projectData = {};
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
-const app = express()
+//Body-Parser
+const bodyParser = require('body-parser');
 
+//Express
+const app = express()
 app.use(express.static('dist'))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Cors
+const cors = require('cors');
+app.use(cors());
 
 console.log(__dirname)
 
 // Require the Aylien npm package
 var aylien = require("aylien_textapi");
 var textapi = new AYLIENTextAPI({
-    application_id: "YOUR_APP_ID",
-    application_key: "YOUR_APP_KEY"
+    application_id: `${process.env.API_ID}`,
+    application_key: `${process.env.API_KEY}`
   });
-
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
+//GET request
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+//POST request
+app.post('/test', function (request, response) {
+    let nameURL = request.body.input.url;
+    console.log(request.body.input.url);
+    textapi.sentiment({
+        url: `${nameURL}`
+    }, function (error, response) {
+        if (error === null) {
+            projectData['polarity'] = response.polarity;
+            projectData['subjectivity'] = response.subjectivity;
+            projectData['polarity_confidence'] = response.polarity_confidence;
+            projectData['subjectivity_confidence'] = response.subjectivity_confidence;
+            return projectData;
+        }
+    });
+    response.send(projectData);
+});
+
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
