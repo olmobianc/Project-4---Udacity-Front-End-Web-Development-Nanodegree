3d8a7b3709288df1ffa66b951fc6beb3ec546b0c
olmo <olmo1995@gmail.com>
2020-08-19 16:04:54 +0200

    fix: all tests now work fine

--- __test__/validURL.spec.js
+++ __test__/validURL.spec.js
@@ -1,10 +1,19 @@
 // Import the js file to test
 import { validUrl } from "../src/client/js/validURL"
 
-describe('Testing url validation functionality', function () {
-    test('should match the expected URL', function () {
+describe('Testing url validation functionality for legitimate urls', function () {
+    test('it should match the expected URL', function () {
       const urlRGEX = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
       const urlTest = 'http://google.com/'; //accepted URL
       expect(urlRGEX.test(urlTest)).toBe(true);
     });
   });
+
+describe('Testing url validation functionality for illegitimate urls' , () => {
+    var url = "Hello World"; //not accepted URL
+    test('It should return true', () => {
+      const response = validUrl(url);
+      expect(response).toBeDefined();
+      expect(response).toBeFalsy();
+    });
+});  
