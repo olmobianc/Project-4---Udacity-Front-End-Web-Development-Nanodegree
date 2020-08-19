530d5edbbb18d11329267a206d289b9b16cdaff6
olmo <olmo1995@gmail.com>
2020-08-19 15:33:05 +0200

    feat: added complete tests

--- __test__/formHandler.spec.js
+++ __test__/formHandler.spec.js
@@ -2,13 +2,13 @@
 import { handleSubmit } from "../src/client/js/formHandler"
 
 describe('Test, the function "handleSubmit()" should exist' , () => {
-    test('It should return true', async () => {
+    test('It should return true', () => {
         expect(handleSubmit).toBeDefined();
     });
 });
 
 describe('Test, the function "handleSubmit()" should be a function', () => {
-    test('It should be a function', async () => {
+    test('It should be a function', () => {
         expect (typeof handleSubmit).toBe('function');
     });
 });
--- __test__/validURL.spec.js
+++ __test__/validURL.spec.js
@@ -1,17 +1,6 @@
 // Import the js file to test
 import { validURL } from "../src/client/js/validURL"
 
-/*
-describe('Testing url validation functionality' , () => {
-    var url = "http://en.wikipedia.org/wiki/Star_Trek&sentences=5";
-    test("Testing the validURL() function for legit urls", async () => {
-        const response = validURL(url);
-        expect(response).toBeDefined();
-        expect(response).toBeTruthy();
-    });
-});
-*/
-
 describe('Testing url validation functionality', function () {
     test.only('should match the expected URL', function () {
       const urlRGEX = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
@@ -22,7 +11,7 @@ describe('Testing url validation functionality', function () {
 
 describe('Testing validURL() function for illegitimate urls' , () => {
     var url = "jkhdfsjkhsjdak.sdahiuhIU";
-    test('It should return true', async () => {
+    test('It should return true', () => {
         const response = validURL(url);
         expect(response).toBeDefined();
         expect(response).toBeFalsy();
