7b6109f7f7fd112885a41ce3926dfdeac0f776d9
olmo <olmo1995@gmail.com>
2020-08-19 11:43:18 +0200

    feat: added test validURL

--- __test__/validURL.spec.js
+++ __test__/validURL.spec.js
@@ -1,14 +1,30 @@
 // Import the js file to test
 import { validURL } from "../src/client/js/validURL"
 
-// The describe() function takes two arguments - a string description, and a test suite as a callback function.  
-// A test suite may contain one or more related tests    
-describe("Testing the submit functionality", () => {
-    // The test() function has two arguments - a string description, and an actual test as a callback function.  
-    test("Testing the validURL() function", () => {
-           // Define the input for the function, if any, in the form of variables/array
-           // Define the expected output, if any, in the form of variables/array
-           // The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
-           // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
-           expect(validURL).toBeDefined();
-})});
+/*
+describe('Testing url validation functionality' , () => {
+    var url = "http://en.wikipedia.org/wiki/Star_Trek&sentences=5";
+    test("Testing the validURL() function for legit urls", async () => {
+        const response = validURL(url);
+        expect(response).toBeDefined();
+        expect(response).toBeTruthy();
+    });
+});
+*/
+
+describe('Testing url validation functionality', function () {
+    test.only('should match the expected URL', function () {
+      const urlRGEX = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
+      const urlTest = 'http://google.com/';
+      expect(urlRGEX.test(urlTest)).toBe(true);
+    });
+  });
+
+describe('Testing validURL() function for illegitimate urls' , () => {
+    var url = "jkhdfsjkhsjdak.sdahiuhIU";
+    test('It should return true', async () => {
+        const response = validURL(url);
+        expect(response).toBeDefined();
+        expect(response).toBeFalsy();
+    });
+});  
