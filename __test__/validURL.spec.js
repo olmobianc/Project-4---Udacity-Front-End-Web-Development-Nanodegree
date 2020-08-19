76927123b2bb560d198a0b7cd782906fcc0ef2ae
olmo <olmo1995@gmail.com>
2020-08-19 15:39:07 +0200

    fix : fixing the validURL test

--- __test__/validURL.spec.js
+++ __test__/validURL.spec.js
@@ -1,8 +1,8 @@
 // Import the js file to test
-import { validURL } from "../src/client/js/validURL"
+import { validUrl } from "../src/client/js/validURL"
 
 describe('Testing url validation functionality', function () {
-    test.only('should match the expected URL', function () {
+    test('should match the expected URL', function () {
       const urlRGEX = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
       const urlTest = 'http://google.com/';
       expect(urlRGEX.test(urlTest)).toBe(true);
@@ -12,7 +12,7 @@ describe('Testing url validation functionality', function () {
 describe('Testing validURL() function for illegitimate urls' , () => {
     var url = "jkhdfsjkhsjdak.sdahiuhIU";
     test('It should return true', () => {
-        const response = validURL(url);
+        const response = validUrl(url);
         expect(response).toBeDefined();
         expect(response).toBeFalsy();
     });
